cc9896ca455adc71e46de58671e4030a2a8f4e74
jirikadlec2 <jirikadlec2@gmail.com>
2014-07-23 22:29:20 -0600

    re-structuring the snow import

--- gsod/gsod_save_snow.py
+++ gsod/gsod_save_snow.py
@@ -1,4 +1,3 @@
-
 import datetime
 import db_save
 import my_gsod
@@ -10,6 +9,7 @@ save_nodata = False
 
 if save_stations:
     import ulmo
+
     stations = ulmo.ncdc.gsod.get_stations()
     db_save.add_sites(stations)
 else:
@@ -19,28 +19,6 @@ sites_saved = 0
 sites_checked = 0
 sites_error = 0
 year = 2014
-for st in stations:
-    try:
-        ts_list = my_gsod.get_data(st['site_code'], year)
-
-        if ts_list:
-            ts_list2 = []
-            for val in ts_list:
-                if val['snow_depth'] > 900:
-                    if save_nodata:
-                        sno_cm = -9999
-                        ts_list2.append({'time': val['date'], 'site_id': st['site_id'], 'qualifier': 1, 'val': sno_cm })
-                else:
-                    sno_cm = int(round(val['snow_depth'] * 2.54))
-                    val2 = {'time': val['date'], 'site_id': st['site_id'], 'qualifier': 1, 'val': sno_cm }
-                    ts_list2.append(val2)
-
-            if len(ts_list2) > 0:
-                db_save.add_values(ts_list2)
-                sites_saved += 1
-        sites_checked += 1
-    except:
-        sites_error += 1
 
 
 def save_gsod_snow_year(stations, year):
@@ -48,26 +26,31 @@ def save_gsod_snow_year(stations, year):
     sites_checked = 0
     sites_error = 0
     for st in stations:
-    try:
-        ts_list = my_gsod.get_data(st['site_code'], year)
+        try:
+            ts_list = my_gsod.get_data(st['site_code'], year)
 
-        if ts_list:
-            ts_list2 = []
-            for val in ts_list:
-                if val['snow_depth'] > 900:
-                    if save_nodata:
-                        sno_cm = -9999
-                        ts_list2.append({'time': val['date'], 'site_id': st['site_id'], 'qualifier': 1, 'val': sno_cm })
-                else:
-                    sno_cm = int(round(val['snow_depth'] * 2.54))
-                    val2 = {'time': val['date'], 'site_id': st['site_id'], 'qualifier': 1, 'val': sno_cm }
-                    ts_list2.append(val2)
+            if ts_list:
+                ts_list2 = []
+                for val in ts_list:
+                    if val['snow_depth'] > 900:
+                        if save_nodata:
+                            sno_cm = -9999
+                            ts_list2.append(
+                                {'time': val['date'], 'site_id': st['site_id'], 'qualifier': 1, 'val': sno_cm})
+                    else:
+                        sno_cm = int(round(val['snow_depth'] * 2.54))
+                        val2 = {'time': val['date'], 'site_id': st['site_id'], 'qualifier': 1, 'val': sno_cm}
+                        ts_list2.append(val2)
 
-            if len(ts_list2) > 0:
-                db_save.add_values(ts_list2)
-                sites_saved += 1
-        sites_checked += 1
-    except:
-        sites_error += 1
+                if len(ts_list2) > 0:
+                    db_save.add_values(ts_list2)
+                    sites_saved += 1
+            sites_checked += 1
+        except:
+            sites_error += 1
 
     return {'saved': sites_saved, 'checked': sites_checked, 'error': sites_error}
+
+for year in range(2013,1929,-1):
+    save_gsod_snow_year(stations, year)
+    print(year)
