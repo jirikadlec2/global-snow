43bb2b4b6763bfe04de9a45fb37d7be480ab80e9
jirikadlec2 <jirikadlec2@gmail.com>
2014-08-08 17:45:17 -0600

    code to save ghcn data (tested with mysql)
    this must be adjusted also for the postgres database

--- gsod/ghcn_save_snow.py
+++ gsod/ghcn_save_snow.py
@@ -1,11 +1,5 @@
 import ulmo
-import datetime
 import db_save
-
-my_country = 'US'
-begin = '2010-01-01'
-parameter = 'snow_depth'
-save_stations = False
 
 #get all the stations
 all_st = ulmo.ncdc.ghcn_daily.get_stations()
@@ -13,11 +7,12 @@ all_st = ulmo.ncdc.ghcn_daily.get_stations()
 for st_code in all_st:
     st_obj = all_st[st_code]
     print st_obj
-    sno_data = ulmo.ncdc.ghcn_daily.get_data(st_code, elements='SNWD', update=False)
+    sno_data = ulmo.ncdc.ghcn_daily.get_data(st_code, elements='SNWD', update=False, as_dataframe=True)
     if (len(sno_data) > 0):
         
         ts_list2 = []
         sno = sno_data['SNWD']
+        sno2 = sno['value'].dropna()
         
         #now get the site id and save site if required
         site_from_db = db_save.get_site_id(st_code)
@@ -26,36 +21,10 @@ for st_code in all_st:
         else:
             site_id = site_from_db[0]
         #check for NaN!
-        for dat, val in sno.iteritems():
-            if val['value'] != None
-                val2 = {'time': dat, 'site_id': site_id, 'qualifier': 1, 'val': round(val['value']/10)}
-                ts_list2.append(val2)
+        for dat, val in sno2.iteritems():
+            dat2 = dat.start_time.strftime('%Y-%m-%d')
+            val2 = {'time': dat2, 'site_id': site_id, 'qualifier': 1, 'val': round(val/10)}
+            ts_list2.append(val2)
 
         if len(ts_list2) > 0:
-            db_save.add_values(ts_list2[1:2])     
-        
-        break
-
-
-sno_data['SNWD']['1950':'2014'].plot()
-
-
-st = ulmo.ncdc.ghcn_daily.get_stations(country='EZ', as_dataframe=True)
-
-data = ulmo.ncdc.ghcn_daily.get_data('USC00427064', elements='SNWD', update=False, as_dataframe=True)
-
-data_mile = ulmo.ncdc.ghcn_daily.get_data('EZ000011464', as_dataframe=True)
-data_mile['SNWD']['2005':'2014'].plot()
-
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-
-#make this into a pandas time series data frame
-df = pd.DataFrame(ts_dict).set_index('date')
-df.index = pd.DatetimeIndex(df.index)
-df['snow_depth'][df.snow_depth > 900] = np.NaN
-
-plt.figure()
-df[['mean_temp','snow_depth']]['20091201':'20100331'].plot()
+            db_save.add_values(ts_list2)     
