73e4f3e9ef9f0c97480242521cf1ccbc1460e19c
jirikadlec2 <jirikadlec2@gmail.com>
2015-07-22 20:43:31 -0600

    ims R data fetcher: added more error checking, added support for ims v1.2 download, refactored code into 3 functions.

--- ims/ims_test.R
+++ ims/ims_test.R
@@ -46,6 +46,9 @@ snowrastif2 <- raster("ims2015001_4km_GIS_v1.3.tif.gz")
 snowrastif <- raster("ims2015001_4km_GIS_v1.3.tif")
 plot(snowrastif)
 
+####################################################################
+# Initialize Study Area
+####################################################################
 #re-project the snow raster
 projection(snowrastif)
 #output raster for: CZECHIA
@@ -84,33 +87,58 @@ plot(reports, add=TRUE)
 ########################################################
 # FUNCTION to get snow area                            #
 ########################################################
-get_snow_area <- function(date, study_area_raster, country) {
+get_snow_raster <- function(date, study_area_raster) {
   dat <- as.Date(date)
   year <- year(dat)
   jd <- yday(dat)
+  version <- "1.2"
+  ext <- "zip"
+  if (year > 2014 | (year == 2014 & jd > 335)) {
+    version <- "1.3"
+    ext <- "tif.gz"
+  }
   base_uri <- "ftp://sidads.colorado.edu/DATASETS/NOAA/G02156/GIS/4km"
-  url <- sprintf("%s/%d/ims%d%03d_4km_GIS_v1.3.tif.gz",
-                 base_uri, year, year, jd)
-  gzfile <- sprintf("ims%d%03d_4km_GIS_v1.3.tif.gz", year, jd)
-  download.file(url, gzfile)
-  #unzip the gz file
-  rasterfile <- gunzip(gzfile, overwrite=TRUE)
+  url <- sprintf("%s/%d/ims%d%03d_4km_GIS_v%s.%s",
+                 base_uri, year, year, jd, version, ext)
+  gzfile <- sprintf("ims%d%03d_4km_GIS_v%s.%s", year, jd, version, ext)
+  rasterfile <- sprintf("ims%d%03d_4km_GIS_v%s.tif", year, jd, version)
   
+  if (!file.exists(rasterfile)) {
+    #if the file doesn't exist, download and unzip a new *.tif file
+    err <- try(download.file(url, gzfile))
+    if (class(err) == "try-error") return (NULL)
+    
+    #unzip the gz file
+    if (ext == "tif.gz") {
+      gunzip(gzfile, overwrite=TRUE)
+    } else {
+      unzip(gzfile, overwrite=TRUE)
+      print(rasterfile)
+    }
+  } 
+  #read raster from geotiff file
   ims_r <- raster(rasterfile)
   
-  #clip raster by study area
+  #project and clip raster to study area
   snowras <- projectRaster(ims_r, study_area_raster, method="ngb")
-  snowras <- mask(snowras, study_area_raster)
-  plot(snowras)
-  plot(country, add=TRUE)
-  
+  snowras <- (snowras == 4)
+  return (snowras)
+}
+
+get_snow_area <- function(study_area_raster, snow_raster) {
   #calculation of the area
   study_area_cells <- cellStats(study_area_raster, sum)
-  
-  snow_area <- cellStats(snowras == 4, sum)
-  area_percent <- snow_area / czech_area
-  
-  return (area_percent)
+  snowmasked <- mask(snow_raster, study_area_raster)
+  snow_area <- cellStats(snowmasked > 0, sum)
+  area_percent <- snow_area / study_area_cells
+  return (area_percent * 100)
+}
+
+plot_snow <- function(snow_raster, date, area_percent, country) {
+  plot(snow_raster, col=c("lightgreen", "white"), legend=FALSE, 
+       main=paste(date, "snow cover: ", sprintf("%.2f", area_percent), "%")
+       )
+  plot(country, add=TRUE)
 }
 
 ###############################################
@@ -127,24 +155,48 @@ framework <- raster(e)
 res(framework)=cellsize
 projection(framework) <- CRS("+proj=longlat")
 czechras <- rasterize(czechia, framework)
+czech_area <- cellStats(czechras, sum)
 
-start <- "2014-12-02"
+start <- "2013-01-01"
 end <- "2015-03-31"
 dates <- seq(as.Date(start), as.Date(end), by = 1)
+
+reports <- read.csv("../user_reports/snowdata.csv", header = TRUE,
+                    stringsAsFactors = FALSE)
+names(reports) <- c("date", "time", "latitude", "longitude", "site", "snow")
+reports$date <- as.Date(reports$date)
 
 N <- length(dates)
 ims_snow_area <- rep(0, N)
 for (i in 1: N) {
-  ims_snow_area[i] <- get_snow_area(dates[i], czechras, czechia)
+  #fetch snow raster
+  snow_raster <- get_snow_raster(dates[i], czechras)
+  if (is.null(snow_raster)) next #skip if download failed
+  
+  #calc area covered by snow
+  ims_snow_area[i] <- get_snow_area(czechras, snow_raster)
+  
+  #plot snow raster
+  plot_snow(snow_raster, dates[i], ims_snow_area[i], czechia)
+  
+  #also plot the active sites and export the raster
+  active_reports <- reports[reports$date == dates[i],]
+  if (nrow(active_reports) == 0) {
+    next
+  }
+  coordinates(active_reports) <- ~longitude+latitude
+  nonzero_reports <- active_reports[active_reports$snow > 0.5,]
+  points(nonzero_reports)
+  if (nrow(nonzero_reports) == 0) {
+    next
+  }
+  text(nonzero_reports, labels = nonzero_reports$snow, pos=4)
 }
 
 #################################################################
 # TEST: compare with number of ground volunteer reports from CZ #
 #################################################################
-reports <- read.csv("../user_reports/snowdata.csv", header = TRUE,
-                    stringsAsFactors = FALSE)
-names(reports) <- c("date", "time", "latitude", "longitude", "site", "snow")
-reports$date <- as.Date(reports$date)
+
 reports2 <- reports[reports$date >= start,]
 reports3 <- reports2[reports2$snow > 0,]
 reports4 <- data.frame(date=reports3$date, snow=reports3$snow)
@@ -157,10 +209,4 @@ plot(dates, ims_snow_area, type="l")
 plot(snow~date, data=reports_count, col="red", type="l")
 par(mfrow=c(1,1))
 
-#scatterplot
-snow_ims <- data.frame(date = dates, ims_snow = ims_snow_area)
-reg <- merge(snow_ims, reports_count, by.x="date", by.y="date")
-plot(snow~ims_snow, data=reg, ylab="#snow reports", xlab="IMS snow covered area")
-m <- lm(snow~ims_snow, data=reg)
-summary(m)
-abline(m, col="red")
+ 
