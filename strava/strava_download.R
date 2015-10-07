e8060bccdaa6e305586d1d9b153f3b6f7381bab5
jirikadlec2 <jirikadlec2@gmail.com>
2015-10-06 21:41:32 -0600

    strava downloading activities

--- strava/strava_download.R
+++ strava/strava_download.R
@@ -20,14 +20,29 @@ parseTcx <- function(my_file) {
 }
 
 ############################################################
-activity_id <- 256120402
-url <- paste("http://raceshape.com/strava.export.php?ride=",
-             activity_id, "&type=tcx", sep="")
-my_file <- paste(activity_id, ".tcx", sep="")
-download.file(url, destfile=my_file)
-Sys.sleep(30)
-download.file(url, destfile=my_file)
-track <- parseTcx(my_file)
-
+getActivity <- function(activity_id) {
+  #activity_id <- 269583360
+  url <- paste("http://raceshape.com/strava.export.php?ride=",
+               activity_id, "&type=tcx", sep="")
+  my_file <- paste(activity_id, ".tcx", sep="")
+  download.file(url, destfile=my_file)
+  Sys.sleep(30)
+  download.file(url, destfile=my_file)
+  track <- parseTcx(my_file)
+}
 ############################################################
 
+############################################################
+# HTML parsing for STRAVA activities
+############################################################
+#activities_file <- download.file(destfile="activities.html", url="https://www.strava.com/activities/search?activity_type=NordicSki&city=Prague&country=Czech+Republic&distance_end=200&distance_start=0&elev_gain_end=15000&elev_gain_start=0&keywords=&lat_lng=50.0755381%2C14.43780049999998&location=Praha&page=1&state=Hlavn%C3%AD+m%C4%9Bsto+Praha&time_end=10.0&time_start=0.0&type=&utf8=%E2%9C%93")
+html.raw <- htmlTreeParse("lists/cheb_WinterSport_1.html", useInternalNodes=TRUE)
+html.parse <- xpathApply(html.raw, "//a", xmlAttrs)
+activ.parse <- grep('*/activities/[0-9]+', unlist(html.parse), value=TRUE)
+
+i <- 1
+for (i in 1: length(activ.parse)) {
+  activ.parse[i]
+  activity_id <- as.numeric(substr(activ.parse[i], 13, 23))
+  getActivity(activity_id)
+}
